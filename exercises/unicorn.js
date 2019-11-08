4f8510b0d615629cd04499ee7a6b435900bcbc27
nancylee713 <srlee713@gmail.com>
2019-11-08 10:11:07 -0700

    Get started with unicorn js

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,7 +1,22 @@
 class Unicorn {
   // Think of a constructor as being similar to initialize in Ruby
   // Instead of setting attributes like @name = name in JavaScript we say this.name = name
-  constructor() {
+  constructor(name, color) {
+    this.name = name
+    this.color = color || 'white'
+  }
+
+  isWhite() {
+    if (this.color == 'white') {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  says(str) {
+    // return '**;* ' + str + ' *;**'
+    return `**\;* ${str} *\;**`
   }
 }
 
