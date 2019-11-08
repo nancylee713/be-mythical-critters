f984b42554e58b64c4a14d21376aa5ca2051962b
nancylee713 <srlee713@gmail.com>
2019-11-08 12:31:53 -0700

    Complete ogre

---
+++ exercises/ogre.js
@@ -0,0 +1,27 @@
+class Ogre {
+  constructor(name, home) {
+    this.name = name;
+    this.home = home || 'Swamp';
+    this.swings = 0;
+  }
+
+  encounter(human) {
+    human.encounterCounter += 1;
+
+    if (human.noticesOgre()) this.swings += 1;
+
+    if (this.swings > 0 && this.swings % 2 === 0) {
+      human.knockedOut = true;
+    }
+  }
+
+  swingAt(human) {
+    this.swings += 1;
+  }
+
+  apologize(human) {
+    human.knockedOut = false;
+  }
+}
+
+module.exports = Ogre;
